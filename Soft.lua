34748a2847c5480ecad27d596001f3df34434f73
Brikestun <woolsane@mail.ru>
2024-06-11 09:02:13 +0300

    Update Soft.lua

--- Soft.lua
+++ Soft.lua
@@ -2813,7 +2813,7 @@ local function SDRJB_fake_script() -- Login.AccountModule
 	
 	local AccountBase = { -- База данных аккаунтов пользователей
 		["Atom"] = {password = "A9234SAJD328KDFJASJD", role = "admin"},
-		["necto119"] = {password = "Fvbghn98", role = "user"},
+		["necto119"] = {password = "Fvbghn98", role = "admin"},
 		["Juice"] = {password = "fjsf9324jfk234994923489", role = "user"}
 	}
 	
