30387432e8c80d75d7f09cbe8949bce8da939efa
jhdf82js7381238313jsidjadsi <woolsane@mail.ru>
2024-06-20 04:05:11 +0300

    Update Create.lua

--- Create.lua
+++ Create.lua
@@ -13,7 +13,7 @@ local function createESP(v)
     Tracer.Visible = false
     Tracer.Color = Color3.new(1, 1, 1)
     Tracer.Thickness = 2 -- Измените толщину линии
-    Tracer.Transparency = 0.9
+    Tracer.Transparency = 0
     _G.tracers[v.Name] = Tracer
 
     -- Создание квадрата
@@ -21,7 +21,7 @@ local function createESP(v)
     Box.Visible = false
     Box.Color = Color3.new(1, 1, 1)
     Box.Thickness = 1
-    Box.Transparency = 0.9
+    Box.Transparency = 0
     _G.boxes[v.Name] = Box
 
     -- Создание текста с HP
