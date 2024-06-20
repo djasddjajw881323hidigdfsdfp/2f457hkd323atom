7b86a2eac50209dc536264c672ddc767c7b7f889
jhdf82js7381238313jsidjadsi <woolsane@mail.ru>
2024-06-20 04:16:15 +0300

    Update Create.lua

--- Create.lua
+++ Create.lua
@@ -3,20 +3,11 @@ local camera = game:GetService("Workspace").CurrentCamera
 
 _G.TeamCheck = false -- Используйте true или false для переключения проверки команды
 
-_G.tracers = {}
 _G.boxes = {}
 _G.texts = {}
 
 local function createESP(v)
-    -- Создание линии-трассера
-    local Tracer = Drawing.new("Line")
-    Tracer.Visible = false
-    Tracer.Color = Color3.fromRGB(255, 255, 255)
-    Tracer.Thickness = 1 -- Измените толщину линии
-    Tracer.Transparency = 1
-    _G.tracers[v.Name] = Tracer
-
-    -- Создание квадрата
+    -- Создание квадрата (хитбокса)
     local Box = Drawing.new("Quad")
     Box.Visible = false
     Box.Color = Color3.fromRGB(255, 255, 255)
@@ -34,26 +25,16 @@ local function createESP(v)
     local function updateESP()
         while true do
             if v.Character and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("HumanoidRootPart") and v ~= lplr and v.Character.Humanoid.Health > 0 then
-                local Vector, OnScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                local Vector, OnScreen = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
                 local RootPart = v.Character.HumanoidRootPart
-                local Head = v.Character:FindFirstChild("Head")
                 local HRPSize = Vector3.new(5, 5, 0) -- Размер квадрата
 
                 if OnScreen then
-                    -- Обновление трассера
-                    Tracer.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y)
-                    Tracer.To = Vector2.new(Vector.X, Vector.Y)
-                    if _G.TeamCheck and v.TeamColor == lplr.TeamColor then
-                        Tracer.Visible = false
-                    else
-                        Tracer.Visible = true
-                    end
-
-                    -- Обновление квадрата
-                    local TopLeft = camera:worldToViewportPoint((RootPart.CFrame * CFrame.new(HRPSize.X / 2, HRPSize.Y / 2, 0)).Position)
-                    local TopRight = camera:worldToViewportPoint((RootPart.CFrame * CFrame.new(-HRPSize.X / 2, HRPSize.Y / 2, 0)).Position)
-                    local BottomLeft = camera:worldToViewportPoint((RootPart.CFrame * CFrame.new(HRPSize.X / 2, -HRPSize.Y / 2, 0)).Position)
-                    local BottomRight = camera:worldToViewportPoint((RootPart.CFrame * CFrame.new(-HRPSize.X / 2, -HRPSize.Y / 2, 0)).Position)
+                    -- Обновление квадрата (хитбокса)
+                    local TopLeft = camera:WorldToViewportPoint((RootPart.CFrame * CFrame.new(HRPSize.X / 2, HRPSize.Y / 2, 0)).Position)
+                    local TopRight = camera:WorldToViewportPoint((RootPart.CFrame * CFrame.new(-HRPSize.X / 2, HRPSize.Y / 2, 0)).Position)
+                    local BottomLeft = camera:WorldToViewportPoint((RootPart.CFrame * CFrame.new(HRPSize.X / 2, -HRPSize.Y / 2, 0)).Position)
+                    local BottomRight = camera:WorldToViewportPoint((RootPart.CFrame * CFrame.new(-HRPSize.X / 2, -HRPSize.Y / 2, 0)).Position)
 
                     Box.PointA = Vector2.new(TopRight.X, TopRight.Y)
                     Box.PointB = Vector2.new(TopLeft.X, TopLeft.Y)
@@ -61,17 +42,15 @@ local function createESP(v)
                     Box.PointD = Vector2.new(BottomRight.X, BottomRight.Y)
                     Box.Visible = true
 
-                    -- Обновление текста
+                    -- Обновление текста с HP
                     Text.Position = Vector2.new(Vector.X, Vector.Y - 30)
                     Text.Text = "["..tostring(math.floor(v.Character.Humanoid.Health)) .. " / " .. tostring(math.floor(v.Character.Humanoid.MaxHealth)).."]"
                     Text.Visible = true
                 else
-                    Tracer.Visible = false
                     Box.Visible = false
                     Text.Visible = false
                 end
             else
-                Tracer.Visible = false
                 Box.Visible = false
                 Text.Visible = false
             end
