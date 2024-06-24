9e2e3d13e1654123408d2a63d384ff1c7a8ccad3
jhdf82js7381238313jsidjadsi <woolsane@mail.ru>
2024-06-24 08:37:16 +0300

    Update ButtonsDownloader.lua

--- ButtonsDownloader.lua
+++ ButtonsDownloader.lua
@@ -1,1 +1,540 @@
+local Debugger = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local ImageLabel = Instance.new("ImageLabel")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local Button = Instance.new("TextButton")
+local Game = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local UICorner_4 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local Button_2 = Instance.new("TextButton")
+local Home = Instance.new("Frame")
+local UICorner_5 = Instance.new("UICorner")
+local ImageLabel_3 = Instance.new("ImageLabel")
+local UICorner_6 = Instance.new("UICorner")
+local TextLabel_3 = Instance.new("TextLabel")
+local Button_3 = Instance.new("TextButton")
+local Player = Instance.new("Frame")
+local UICorner_7 = Instance.new("UICorner")
+local ImageLabel_4 = Instance.new("ImageLabel")
+local UICorner_8 = Instance.new("UICorner")
+local TextLabel_4 = Instance.new("TextLabel")
+local Button_4 = Instance.new("TextButton")
+local Title1 = Instance.new("Frame")
+local TextLabel_5 = Instance.new("TextLabel")
+local Title2 = Instance.new("Frame")
+local TextLabel_6 = Instance.new("TextLabel")
+local Updates = Instance.new("Frame")
+local UICorner_9 = Instance.new("UICorner")
+local ImageLabel_5 = Instance.new("ImageLabel")
+local UICorner_10 = Instance.new("UICorner")
+local TextLabel_7 = Instance.new("TextLabel")
+local Button_5 = Instance.new("TextButton")
+local Config = Instance.new("Frame")
+local UICorner_11 = Instance.new("UICorner")
+local ImageLabel_6 = Instance.new("ImageLabel")
+local UICorner_12 = Instance.new("UICorner")
+local TextLabel_8 = Instance.new("TextLabel")
+local Button_6 = Instance.new("TextButton")
 
+--Properties:
+
+Debugger.Name = "Debugger"
+Debugger.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Debugger.BackgroundTransparency = 1.000
+Debugger.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Debugger.BorderSizePixel = 0
+Debugger.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner.Parent = Debugger
+
+ImageLabel.Parent = Debugger
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel.Image = "rbxassetid://18110868029"
+ImageLabel.ScaleType = Enum.ScaleType.Crop
+
+UICorner_2.Parent = ImageLabel
+
+TextLabel.Parent = Debugger
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel.Size = UDim2.new(0, 168, 0, 26)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "A.Debugger"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 30.000
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+Button.Name = "Button"
+Button.Parent = Debugger
+Button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button.BackgroundTransparency = 1.000
+Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button.BorderSizePixel = 0
+Button.Size = UDim2.new(0, 218, 0, 38)
+Button.Font = Enum.Font.SourceSans
+Button.Text = ""
+Button.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button.TextSize = 14.000
+
+Game.Name = "Game"
+Game.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Game.BackgroundTransparency = 1.000
+Game.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Game.BorderSizePixel = 0
+Game.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner_3.Parent = Game
+
+ImageLabel_2.Parent = Game
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.BackgroundTransparency = 1.000
+ImageLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel_2.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel_2.Image = "rbxassetid://18109297031"
+ImageLabel_2.ScaleType = Enum.ScaleType.Crop
+
+UICorner_4.Parent = ImageLabel_2
+
+TextLabel_2.Parent = Game
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel_2.Size = UDim2.new(0, 168, 0, 26)
+TextLabel_2.Font = Enum.Font.SourceSansBold
+TextLabel_2.Text = "Place"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 30.000
+TextLabel_2.TextWrapped = true
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+
+Button_2.Name = "Button"
+Button_2.Parent = Game
+Button_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button_2.BackgroundTransparency = 1.000
+Button_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button_2.BorderSizePixel = 0
+Button_2.Size = UDim2.new(0, 218, 0, 38)
+Button_2.Font = Enum.Font.SourceSans
+Button_2.Text = ""
+Button_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button_2.TextSize = 14.000
+
+Home.Name = "Home"
+Home.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Home.BackgroundTransparency = 1.000
+Home.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Home.BorderSizePixel = 0
+Home.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner_5.Parent = Home
+
+ImageLabel_3.Parent = Home
+ImageLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_3.BackgroundTransparency = 1.000
+ImageLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_3.BorderSizePixel = 0
+ImageLabel_3.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel_3.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel_3.Image = "rbxassetid://18109467543"
+ImageLabel_3.ScaleType = Enum.ScaleType.Crop
+
+UICorner_6.Parent = ImageLabel_3
+
+TextLabel_3.Parent = Home
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel_3.Size = UDim2.new(0, 158, 0, 26)
+TextLabel_3.Font = Enum.Font.SourceSansBold
+TextLabel_3.Text = "Main"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextSize = 30.000
+TextLabel_3.TextWrapped = true
+TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+
+Button_3.Name = "Button"
+Button_3.Parent = Home
+Button_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button_3.BackgroundTransparency = 1.000
+Button_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button_3.BorderSizePixel = 0
+Button_3.Size = UDim2.new(0, 218, 0, 38)
+Button_3.Font = Enum.Font.SourceSans
+Button_3.Text = ""
+Button_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button_3.TextSize = 14.000
+
+Player.Name = "Player"
+Player.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Player.BackgroundTransparency = 1.000
+Player.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Player.BorderSizePixel = 0
+Player.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner_7.Parent = Player
+
+ImageLabel_4.Parent = Player
+ImageLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_4.BackgroundTransparency = 1.000
+ImageLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_4.BorderSizePixel = 0
+ImageLabel_4.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel_4.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel_4.Image = "rbxassetid://18108988137"
+ImageLabel_4.ScaleType = Enum.ScaleType.Crop
+
+UICorner_8.Parent = ImageLabel_4
+
+TextLabel_4.Parent = Player
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel_4.Size = UDim2.new(0, 158, 0, 26)
+TextLabel_4.Font = Enum.Font.SourceSansBold
+TextLabel_4.Text = "Player"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextSize = 30.000
+TextLabel_4.TextWrapped = true
+TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
+
+Button_4.Name = "Button"
+Button_4.Parent = Player
+Button_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button_4.BackgroundTransparency = 1.000
+Button_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button_4.BorderSizePixel = 0
+Button_4.Size = UDim2.new(0, 218, 0, 38)
+Button_4.Font = Enum.Font.SourceSans
+Button_4.Text = ""
+Button_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button_4.TextSize = 14.000
+
+Title1.Name = "Title1"
+Title1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title1.BackgroundTransparency = 1.000
+Title1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Title1.BorderSizePixel = 0
+Title1.Size = UDim2.new(0, 214, 0, 42)
+
+TextLabel_5.Parent = Title1
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.0116822431, 0, 0.166666672, 0)
+TextLabel_5.Size = UDim2.new(0, 198, 0, 26)
+TextLabel_5.Font = Enum.Font.SourceSansBold
+TextLabel_5.Text = "Opportunities"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 30.000
+TextLabel_5.TextTransparency = 0.600
+TextLabel_5.TextWrapped = true
+TextLabel_5.TextXAlignment = Enum.TextXAlignment.Left
+
+Title2.Name = "Title2"
+Title2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title2.BackgroundTransparency = 1.000
+Title2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Title2.BorderSizePixel = 0
+Title2.Size = UDim2.new(0, 214, 0, 42)
+
+TextLabel_6.Parent = Title2
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BackgroundTransparency = 1.000
+TextLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.0116822431, 0, 0.166666672, 0)
+TextLabel_6.Size = UDim2.new(0, 198, 0, 26)
+TextLabel_6.Font = Enum.Font.SourceSansBold
+TextLabel_6.Text = "Client"
+TextLabel_6.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 30.000
+TextLabel_6.TextTransparency = 0.600
+TextLabel_6.TextWrapped = true
+TextLabel_6.TextXAlignment = Enum.TextXAlignment.Left
+
+Updates.Name = "Updates"
+Updates.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Updates.BackgroundTransparency = 1.000
+Updates.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Updates.BorderSizePixel = 0
+Updates.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner_9.Parent = Updates
+
+ImageLabel_5.Parent = Updates
+ImageLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_5.BackgroundTransparency = 1.000
+ImageLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_5.BorderSizePixel = 0
+ImageLabel_5.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel_5.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel_5.Image = "rbxassetid://18186036078"
+ImageLabel_5.ScaleType = Enum.ScaleType.Crop
+
+UICorner_10.Parent = ImageLabel_5
+
+TextLabel_7.Parent = Updates
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.BackgroundTransparency = 1.000
+TextLabel_7.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.BorderSizePixel = 0
+TextLabel_7.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel_7.Size = UDim2.new(0, 168, 0, 26)
+TextLabel_7.Font = Enum.Font.SourceSansBold
+TextLabel_7.Text = "Updates"
+TextLabel_7.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.TextSize = 30.000
+TextLabel_7.TextWrapped = true
+TextLabel_7.TextXAlignment = Enum.TextXAlignment.Left
+
+Button_5.Name = "Button"
+Button_5.Parent = Updates
+Button_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button_5.BackgroundTransparency = 1.000
+Button_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button_5.BorderSizePixel = 0
+Button_5.Size = UDim2.new(0, 218, 0, 38)
+Button_5.Font = Enum.Font.SourceSans
+Button_5.Text = ""
+Button_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button_5.TextSize = 14.000
+
+Config.Name = "Config"
+Config.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Config.BackgroundTransparency = 1.000
+Config.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Config.BorderSizePixel = 0
+Config.Size = UDim2.new(0, 214, 0, 42)
+
+UICorner_11.Parent = Config
+
+ImageLabel_6.Parent = Config
+ImageLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_6.BackgroundTransparency = 1.000
+ImageLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_6.BorderSizePixel = 0
+ImageLabel_6.Position = UDim2.new(0.0093457941, 0, 0.0476190485, 0)
+ImageLabel_6.Size = UDim2.new(0, 35, 0, 35)
+ImageLabel_6.Image = "rbxassetid://18114172241"
+ImageLabel_6.ScaleType = Enum.ScaleType.Crop
+
+UICorner_12.Parent = ImageLabel_6
+
+TextLabel_8.Parent = Config
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.BackgroundTransparency = 1.000
+TextLabel_8.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.BorderSizePixel = 0
+TextLabel_8.Position = UDim2.new(0.200934574, 0, 0.166666672, 0)
+TextLabel_8.Size = UDim2.new(0, 168, 0, 26)
+TextLabel_8.Font = Enum.Font.SourceSansBold
+TextLabel_8.Text = "Config"
+TextLabel_8.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.TextSize = 30.000
+TextLabel_8.TextWrapped = true
+TextLabel_8.TextXAlignment = Enum.TextXAlignment.Left
+
+Button_6.Name = "Button"
+Button_6.Parent = Config
+Button_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button_6.BackgroundTransparency = 1.000
+Button_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Button_6.BorderSizePixel = 0
+Button_6.Size = UDim2.new(0, 218, 0, 38)
+Button_6.Font = Enum.Font.SourceSans
+Button_6.Text = ""
+Button_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+Button_6.TextSize = 14.000
+
+-- Scripts:
+
+local function CTMKH_fake_script() -- Debugger.CoreClient 
+	local script = Instance.new('LocalScript', Debugger)
+
+	local Button = script.Parent.Button
+	local WarningFrame = script.Parent.Parent.Parent.Parent.Warning
+	local YesButton = WarningFrame.Yes
+	local NoButton = WarningFrame.No
+	
+	local Background = script.Parent
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		WarningFrame.Visible = true
+	end)
+	YesButton.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/djasddjajw881323hidigdfsdfp/2f457hkd323atom/atom/A.Debugger"))()
+		WarningFrame.Visible = false
+	end)
+	NoButton.MouseButton1Click:Connect(function()
+		WarningFrame.Visible = false
+	end)
+end
+local function CKIWU_fake_script() -- Game.CoreClient 
+	local script = Instance.new('LocalScript', Game)
+
+	local Button = script.Parent.Button
+	local Background = script.Parent
+	local PlayerFrame = script.Parent.Parent.Parent.Parent.Main.PlayerFr
+	local GameFrame = script.Parent.Parent.Parent.Parent.Main.PlaceFr
+	local MainFrame = script.Parent.Parent.Parent.Parent.Main.MainFrame
+	local ConfigFrame = script.Parent.Parent.Parent.Parent.Main.Config
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		PlayerFrame.Visible = false
+		GameFrame.Visible = true
+		MainFrame.Visible = false
+		ConfigFrame.Visible = false
+	end)
+end
+local function BCXCSJ_fake_script() -- Home.CoreClient 
+	local script = Instance.new('LocalScript', Home)
+
+	local Button = script.Parent.Button
+	local Background = script.Parent
+	local MainFrame = script.Parent.Parent.Parent.Parent.Main.MainFrame
+	local PlayerFrame = script.Parent.Parent.Parent.Parent.Main.PlayerFr
+	local GameFrame = script.Parent.Parent.Parent.Parent.Main.PlaceFr
+	local ConfigFrame = script.Parent.Parent.Parent.Parent.Main.Config
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		PlayerFrame.Visible = false
+		GameFrame.Visible = false
+		MainFrame.Visible = true
+		ConfigFrame.Visible = false
+	end)
+end
+local function EEVTM_fake_script() -- Player.CoreClient 
+	local script = Instance.new('LocalScript', Player)
+
+	local Button = script.Parent.Button
+	local Background = script.Parent
+	local PlayerFrame = script.Parent.Parent.Parent.Parent.Main.PlayerFr
+	local GameFrame = script.Parent.Parent.Parent.Parent.Main.PlaceFr
+	local MainFrame = script.Parent.Parent.Parent.Parent.Main.MainFrame
+	local ConfigFrame = script.Parent.Parent.Parent.Parent.Main.Config
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		PlayerFrame.Visible = true
+		GameFrame.Visible = false
+		MainFrame.Visible = false
+		ConfigFrame.Visible = false
+	end)
+end
+local function KLQY_fake_script() -- Updates.CoreClient 
+	local script = Instance.new('LocalScript', Updates)
+
+	local Button = script.Parent.Button
+	local Background = script.Parent
+	local StarterGui = game:GetService("StarterGui")
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		StarterGui:SetCore("SendNotification", {
+			Title = "Atom - Client Updates",
+			Text = "You already have the latest version",
+			Duration = 5,
+		})
+	end)
+end
+local function JZKEJHO_fake_script() -- Config.CoreClient 
+	local script = Instance.new('LocalScript', Config)
+
+	local Button = script.Parent.Button
+	local Background = script.Parent
+	local PlayerFrame = script.Parent.Parent.Parent.Parent.Main.PlayerFr
+	local GameFrame = script.Parent.Parent.Parent.Parent.Main.PlaceFr
+	local MainFrame = script.Parent.Parent.Parent.Parent.Main.MainFrame
+	local ConfigFrame = script.Parent.Parent.Parent.Parent.Main.Config
+	
+	Button.MouseEnter:Connect(function()
+		Background.BackgroundTransparency = 0.9
+	end)
+	Button.MouseLeave:Connect(function()
+		Background.BackgroundTransparency = 1
+	end)
+	Button.MouseButton1Click:Connect(function()
+		PlayerFrame.Visible = false
+		GameFrame.Visible = false
+		MainFrame.Visible = false
+		ConfigFrame.Visible = true
+	end)
+end
+
+wait(1)
+
+Title1.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Home.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Game.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Player.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Title2.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Debugger.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Config.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+wait(0.1)
+Updates.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
+
+wait(1)
+
+coroutine.wrap(CTMKH_fake_script)()
+coroutine.wrap(CKIWU_fake_script)()
+coroutine.wrap(BCXCSJ_fake_script)()
+coroutine.wrap(EEVTM_fake_script)()
+coroutine.wrap(KLQY_fake_script)()
+coroutine.wrap(JZKEJHO_fake_script)()
