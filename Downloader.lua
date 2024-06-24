eccee86c3232fd5a929f9fca1715ec237915f823
jhdf82js7381238313jsidjadsi <woolsane@mail.ru>
2024-06-24 06:59:34 +0300

    Update Downloader.lua

--- Downloader.lua
+++ Downloader.lua
@@ -3750,8 +3750,10 @@ local function WLDVPQ_fake_script() -- Gui.Fling.LocalScript
 	
 end
 
-wait(1)
-
+wait(0.1)
+Gui.Game.Parent = game.CoreGui.Atom["Atom.Frane"].Main
+wait(0.1)
+Gui.Place.Parent = game.CoreGui.Atom["Atom.Frane"].Main
 wait(0.1)
 Gui.Title.Parent = game.CoreGui.Atom["Atom.Frame"].Panel.Buttons
 wait(0.1)
